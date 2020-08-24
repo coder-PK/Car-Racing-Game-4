5afb32ed55d6fbb32964f2ce578fd606946a2c29
coder-PK <66046408+coder-PK@users.noreply.github.com>
2020-08-23 21:50:59 -0400

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,50 @@
+class Player {
+  constructor(){
+    this.name = null;
+    this.distance = 0;
+    this.index = null;
+    this.rank = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "Players/players" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance: this.distance 
+    });
+  }
+
+ static getPlayerInfo(){
+    var playerInfo= database.ref("Players");
+    playerInfo.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+
+  }
+   getCarsAtEnd(){
+     database.ref("CarsAtEnd").on("value",(data)=>{
+      this.rank =  data.val();
+     })
+   
+    }
+
+    static updateCarsAtEnd(rank){
+      database.ref("/").update({
+        CarsAtEnd: rank
+      })
+    }
+  }
+
